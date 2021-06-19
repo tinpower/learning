10097b9d6d884a7dcdd5d292865dacd0afd8c5bf
Khai Pham <khai.pnp@outlook.com>
2021-06-19 14:22:28 +0700

    Learn JS

---
+++ Learn-JS/homework/js/index.js
@@ -0,0 +1,38 @@
+// BAI TAP 1
+function salaryMethod() {
+  var oneDaySalary = 100000;
+  var days = document.getElementById('fm1').value;
+  var totalSalary = oneDaySalary * days;
+  document.getElementById("showSalary").innerHTML = totalSalary;
+  console.log(totalSalary)
+}
+
+//  BAI TAP 2
+// var number1 = 5;
+// var number2 = 6;
+// var number3 = 7;
+// var number4 = 8;
+// var number5 = 9;
+
+// var midSum = (number1 + number2 + number3 + number4 + number5) / 5;
+// console.log(midSum);
+
+// BAI TAP 3
+// var usdCurency = 5;
+// var vndCurency = 23500;
+// var exchangeVND = usdCurency * vndCurency;
+// console.log(exchangeVND);
+
+// BAI TAP 4
+// var lengthNumber = 10;
+// var widthNumber = 5;
+// var acgreage = lengthNumber * widthNumber;
+// var perimeter = (lengthNumber + widthNumber) * 2;
+// console.log(acgreage, perimeter);
+
+// BAI TAP 5
+// var number = 47;
+// var unit = number % 10;
+// var ten = Math.floor(number / 10);
+// var sum = unit + ten;
+// console.log(sum);
