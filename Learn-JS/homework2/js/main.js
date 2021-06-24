037e81e0dbbe38bcd7fa08f2c0e708543c4fc18e
Phạm Khải <khaipnp@Phams-MacBook-Air.local>
2021-06-24 23:29:49 +0700

    JS

--- Learn-JS/homework2/js/main.js
+++ Learn-JS/homework2/js/main.js
@@ -65,27 +65,49 @@ function changeCurency2Unit() {
 // BAI TAP 4
 
 let valuePerimeter = 0;
+let valueAcgreage = 0;
 let valueWidth = document.getElementById("width").value;
-    valueWidth = parseFloat(valueWidth);
+valueWidth = parseFloat(valueWidth);
 let valueLength = document.getElementById("length").value;
-    valueLength = parseFloat(valueLength);
+valueLength = parseFloat(valueLength);
 
 function resultPerimeter() {
   // METHOD
-  if (valueWidth > valueLength){
-    document.getElementById('showResultRectangle').innerHTML = "Vui lòng điền chiều dài lớn hơn chiều rộng";
+  if (valueWidth > valueLength) {
+    document.getElementById("showResultRectangle").innerHTML =
+      "Vui lòng điền chiều dài lớn hơn chiều rộng";
   } else {
     valuePerimeter = (valueWidth + valueLength) * 2;
-    document.getElementById('showResultRectangle').innerHTML = "Chu vi hình chữ nhật là: " + valuePerimeter;
+    document.getElementById("showResultRectangle").innerHTML =
+      "Chu vi hình chữ nhật là: " + valuePerimeter;
   }
 }
 
 function resultAcgreage() {
   // METHOD
-  if (valueWidth > valueLength){
-    document.getElementById('showResultRectangle').innerHTML = "Vui lòng điền chiều dài lớn hơn chiều rộng";
+  if (valueWidth > valueLength) {
+    document.getElementById("showResultRectangle").innerHTML =
+      "Vui lòng điền chiều dài lớn hơn chiều rộng";
   } else {
     valueAcgreage = valueWidth * valueLength;
-    document.getElementById('showResultRectangle').innerHTML = "Diện tích hình chữ nhật là: " + valueAcgreage;
+    document.getElementById("showResultRectangle").innerHTML =
+      "Diện tích hình chữ nhật là: " + valueAcgreage;
+  }
+}
+
+function result2Unit() {
+  let number2Unit = document.getElementById("ten").value;
+  number2Unit = parseInt(number2Unit);
+  let total2Unit = 0;
+  let numberUnit = number2Unit % 10;
+  let numberTen = number2Unit / 10;
+  numberTen = Math.floor(numberTen);
+  if (9 < number2Unit < 100) {
+    total2Unit = numberTen + numberUnit;
+    document.getElementById('showResult2Unit').innerHTML = "Ký số hàng chục là " + numberTen + "<br />" + "Ký số hàng đơn vị là " + numberUnit + "<br />" + "Tổng 2 ký số là " + total2Unit;
+  } else if ( number2Unit < 9) {
+    document.getElementById('showResult2Unit').innerHTML = "Vui lòng nhập số lớn hơn 9 và nhỏ hơn 100"
+  } else if (number2Unit > 100){
+    document.getElementById('showResult2Unit').innerHTML = "Vui lòng nhập số lớn hơn 9 và nhỏ hơn 100" 
   }
 }
