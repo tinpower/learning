a0b273eb47c5b3c00be4b14ef42693be8db6cb80
Khai <khaipnp@vinorsoft.com>
2021-07-30 16:28:49 +0700

    update

---
+++ Learn-JS/BC14_BAITAPJSQLNV_PHAM_NGUYEN_PHUONG_KHAI/js/main.js
@@ -0,0 +1,83 @@
+const NhanVien = function (
+  username,
+  name,
+  email,
+  password,
+  date,
+  salary,
+  position,
+  workHour
+) {
+  this.username = username;
+  this.name = name;
+  this.email = email;
+  this.password = password;
+  this.date = date;
+  this.salary = salary;
+  this.position = position;
+  this.workHour = workHour;
+};
+
+NhanVien.prototype.calCSalary = () => {
+  if (this.position === "Sếp") {
+    return this.salary * 3 * this.workHour;
+  }
+  if (this.position === "Trưởng phòng") {
+    return this.salary * 2 * this.workHour;
+  }
+  if (this.position === "Nhân viên") {
+    return this.salary * this.workHour;
+  }
+};
+
+const listNhanVien = [];
+
+const getNhanVien = () => {
+  let username = document.getElementById("tknv").value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let date = document.getElementById("datepicker").value;
+  let salary = document.getElementById("luongCB").value;
+  let position = document.getElementById("chucvu").value;
+  let workHour = document.getElementById("gioLam").value;
+
+  let nhanVien = new NhanVien(
+    username,
+    name,
+    email,
+    password,
+    date,
+    salary,
+    position,
+    workHour
+  );
+  console.log(nhanVien);
+  listNhanVien.push(nhanVien);
+  showNhanVien();
+};
+
+const showNhanVien = () => {
+  let tableDanhSach = document.getElementById("tableDanhSach");
+  let html = "";
+  for (let i = 0; i < listNhanVien.length; i++) {
+    html += `
+    <tr>
+      <td>${listNhanVien[i].username}</td>
+      <td>${listNhanVien[i].name}</td>
+      <td>${listNhanVien[i].email}</td>
+      <td>${listNhanVien[i].date}</td>
+      <td>${listNhanVien[i].position}</td>
+      <td>${listNhanVien[i].calCSalary()}</td>
+      <td>${listNhanVien[i].workHour}</td>
+      <td>
+      <button class="btn btn-primary">Cập nhật</button>
+      <button class="btn btn-danger">Xóa</button>
+      </td>
+    </tr>
+    `;
+  }
+  tableDanhSach.innerHTML = html;
+};
+
+document.getElementById("btnThemNV").addEventListener("click", getNhanVien);
---
+++ Learn-JS/BC14_BAITAPJSQLNV_PHAM_NGUYEN_PHUONG_KHAI/js/salary.js
@@ -0,0 +1,12 @@
+const calcSalary = () => {
+  let title = "";
+  if (title === "sếp") {
+    return salary * 3;
+  }
+  if (title === "trưởng phòng") {
+    return salary * 2;
+  }
+  if (title === "nhân viên") {
+    return salary;
+  }
+};
